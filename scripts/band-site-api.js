f7a55722653db3f232650bf43ae46e234500bd76
JustinL197 <159653227+JustinL197@users.noreply.github.com>
2024-04-26 00:02:36 -0400

    updated bio page with appropriate API calls

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,13 +1,47 @@
-class BandSiteApi{
+export const apiKey = "ad81a429-015f-474a-8491-b5c5fe2db636";
+
+export class BandSiteApi{
     constructor(apiKey){
         this.apiKey = apiKey;
-        this.baseUrl = 'https://unit-2-project-api-25c1595833b2.herokuapp.com/';
+        this.baseUrl = 'https://unit-2-project-api-25c1595833b2.herokuapp.com';
     }
 
     async postComment(comment){
-        const response = await axios.post(`${this.baseUrl}/comments?api_key=${this.apiKey}`,
-        comment, {headers: {'Content-Type': 'application/json'}});
+        try {
+            const response = await axios.post(`${this.baseUrl}/comments?api_key=${this.apiKey}`,
+            comment, {headers: {'Content-Type': 'application/json'}});
+    
+            return response.data;
+        }
+        
+        catch(error){
+            console.log('There is an error posting comment:', error.message);
+            //throw error --> ask question regarding this.
+        }
+    }
 
-        return response.data;
+    async getComments(){
+        try{
+            const response = await axios.get(`${this.baseUrl}/comments?api_key=${this.apiKey}`)
+            const sortedComments = response.data.sort((a, b) => b.timestamp - a.timestamp);
+            return sortedComments;
+        }
+
+        catch(error){
+            console.log('There is an error fetching comments:', error.message);
+            //throw error --> ask question regarding this.
+        }
+    }
+
+    async getShows(){
+        try{
+            const response = await axios.get(`${this.baseUrl}/showdates?api_key=${this.apiKey}`)
+            return response.data 
+        }
+
+        catch(error){
+            console.log('There is an error fetching show-dates', error.message);
+            //throw error --> ask quetsion regarding this
+        }
     }
 }
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,8 +1,6 @@
-const commentsArray = [
-    {name:"Victor Pinto", timestamp: "11/02/2023", comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."},
-    {name: "Christina Cabrera", timestamp: "10/28/2023", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."},
-    {name: "Isaac Tadesse", timestamp: "10/20/2023", comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."}
-];
+import { BandSiteApi, apiKey } from "./band-site-api.js";
+
+const bandSiteApi = new BandSiteApi(apiKey);
 
  // function to modify .createElement to add className.
  function createElementWithClass(tagName, className){
@@ -45,27 +43,24 @@ function displayComment(comment){
     commentsSection.appendChild(dividerElem);
 }
 
-commentsArray.forEach(comment => displayComment(comment));
-
 //function to create a comment object from the form
-function createComment(){
+function createCommentObject(){
     
     const nameElem = document.getElementById('name');
     const dateElem = new Date(); 
-    const formatDate = dateElem.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' });
+    // const formatDate = dateElem.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' });
     const commentElem = document.getElementById('comment');
       
     const newComment = {
         name: nameElem.value,
-        timestamp: formatDate,
+        // timestamp: formatDate,
         comment: commentElem.value
     };
-
-    commentsArray.unshift(newComment);
 
     nameElem.classList.remove('comments__invalid');
     commentElem.classList.remove('comments__invalid');
 
+    return newComment;
 }
 
 //function to clear the comments section
@@ -74,11 +69,6 @@ function clearSection(){
     while (commentSection.firstChild){
         commentSection.removeChild(commentSection.firstChild);
     }
-}
-
-//function to rerender comments into the section
-function renderComments(){
-    commentsArray.forEach(comment => displayComment(comment));
 }
 
 //function to reset the form field and submitting
@@ -104,19 +94,36 @@ function checkValidity(){
     return validator;
 }
 
+//function to retrieve comments from backend and display it
+async function retrieveAndDisplayComments(){
+    try{
+       const comments = await bandSiteApi.getComments();
+       clearSection();
+       comments.forEach(comment => displayComment(comment));
+    } catch (error){
+        console.log('Failed to load comments:', error);
+    }
+}
+
+async function handleFormSubmit(event){
+    event.preventDefault();
+    if (!checkValidity()){
+        return false;
+    }
+
+    try{
+        const newComment = createCommentObject();
+        await bandSiteApi.postComment(newComment);
+        await retrieveAndDisplayComments();
+        resetFormField();
+
+    } catch(error){
+        console.log('Failed to post comment:', error);
+    }
+}
+
+retrieveAndDisplayComments();
+
 //on submit, run a function that creates and adds comment to the section
 const submitButton = document.querySelector('.add-comments__form');
-
-submitButton.addEventListener('submit', (event) => {
-   event.preventDefault();
-   if (!checkValidity()){
-    return false;
-   }
-
-   else{
-    createComment();
-    clearSection();
-    renderComments();
-    resetFormField();
-   }
-   });
+submitButton.addEventListener('submit', handleFormSubmit);
